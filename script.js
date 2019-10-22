177d1c6ab685c469fd77b935a881fc452be7e3bf
Alen <Alenslo@gmx.net>
2019-10-23 01:01:10 +0200

    layout and dynamic icon

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 
-
-
-
-
 navigator.geolocation.getCurrentPosition(position => {
    
     let lat = position.coords.latitude; 
@@ -12,30 +8,30 @@ navigator.geolocation.getCurrentPosition(position => {
     .then(response => response.json() )
     .then(data => {
         let div = document.querySelector('#display');
-        
-        let latitude = document.createElement('p') // latitude
-        latitude.innerHTML = 'Latitude: ' + data.coord.lat;
-        div.appendChild(latitude);
 
-        let longitude = document.createElement('p') // longitude
-        longitude.innerHTML = 'Longitude: ' + data.coord.lon;
-        div.appendChild(longitude);
+        let city = document.createElement('h2') // City
+        city.innerHTML = data.name;
+        div.appendChild(city);
 
         let country = document.createElement('p') // country
         country.innerHTML = 'Country: ' + data.sys.country;
         div.appendChild(country);
+        
+        let latitude = document.createElement('p') // latitude
+        latitude.innerHTML = 'Lat: ' + data.coord.lat +' ' + 'Lon: ' + data.coord.lon;
+        div.appendChild(latitude);
 
-        let city = document.createElement('p') // City
-        city.innerHTML = 'City: ' + data.name;
-        div.appendChild(city);
-
-        let temp = document.createElement('p') // Temperature
-        temp.innerHTML = 'Temperature: ' + (data.main.temp - 273.15);
+        let temp = document.createElement('p') // temperature
+        temp.innerHTML = 'Temperature: ' + parseInt((data.main.temp - 273.15)) + '&#8451';
         div.appendChild(temp);
 
-        let description = document.createElement('p') // description
-        description.innerHTML = data.weather[0].main;
-        div.appendChild(description);
+        let main = document.createElement('p') // weather description
+        main.innerHTML = data.weather[0].main;
+        div.appendChild(main);
+
+        console.log(data.weather[0].id)  //get the icon code for current weather condition
+        document.querySelector('i').className = 'wi wi-owm-'+ data.weather[0].id;
+
     });
 });
  
